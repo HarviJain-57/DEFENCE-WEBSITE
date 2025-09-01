2abe0d97d55001f5bbe64fbdd5d050ed1648ba27
HarviJain-57 <harvi.jain@abhyuday.in>
2025-09-01 15:18:14 +0530

    Add requirements.txt and pin Python to 3.11

--- app.py
+++ app.py
@@ -22,5 +22,12 @@ def news():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)
